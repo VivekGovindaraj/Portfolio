a3bef329b37000ca8e37f7ccf6418c9ab87181ab
Vivek G <vivek1679772@gmail.com>
2026-04-26 00:09:01 +0530

    Poject setup Done by:Vivek G

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+
+    <>
+    <div className="bg-black text-white h-secreen flex items-center justify-center">
+      <h1 className="text-4xl fotn-bold">App coming soon</h1>
+
+    </div>
+    </>
+    
+  )
+}
+
+export default App
