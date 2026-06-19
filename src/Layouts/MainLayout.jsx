b8f9b7a04dd8a7bcb1ed6979eaabe6dcde1c5d30
vivekg <vivek1679772@gmail.com>
2026-06-19 23:31:47 +0530

    responsive issues

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -2,28 +2,46 @@ import React from 'react'
 import SideBar from '../Components/Navbar/SideBar';
 import ProfileCard from '../Components/ProfileCard'
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
   return (
-    <div className="bg-bg text-textMain min-h-screen bg-img">
-      
-      <SideBar/> 
-      <div className='flex flex-col lg:flex-row gap-2'>
-        <aside className='w-full lg:w-[360px] lg:fixed'>
-            <ProfileCard/>
-        </aside>
-        
+    <div className="bg-bg text-textMain min-h-screen">
 
-        <main className="w-full px-0 md:px-6 py-6 md:py-8 lg:py-16 md:pl-[150px] lg:pl-[560px] flex-1">
-            {children}
+      <SideBar />
+
+      {/* Mobile & Tablet */}
+      <div className="block lg:hidden">
+
+        <ProfileCard />
+
+        <main>
+          {children}
         </main>
-        
+
       </div>
-      <footer class="px-2 py-2 bottom-0 bg-[#0f0f0f] text-center">
-            <span className='text-md sm:text-sm'>&#x00A9; 2026 VivekCV is powerdby Vivek Govindraj</span>
-      </footer>
-        
+
+      {/* Desktop */}
+      <div className="hidden lg:flex h-screen overflow-hidden">
+
+        {/* Fixed Profile */}
+        <aside
+          className=" w-[420px] shrink-0 flex justify-center pt-16 "
+        >
+          <ProfileCard />
+        </aside>
+
+        {/* Scroll Area */}
+        <main className=" flex-1 overflow-y-auto ps-32 pe-8 py-10 " >
+          {children}
+
+          <footer className="mt-10 py-5 text-center">
+            © 2026 VivekCV
+          </footer>
+        </main>
+
+      </div>
+
     </div>
   )
 }
-  
+
 export default MainLayout;
